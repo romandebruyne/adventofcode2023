c3670ed9d765292f99861e609925e47be0ef8662
Hetterle, Roman (SE-A/35) <roman.hetterle@volkswagen.de>
2023-12-01 13:14:46 +0100

    commit completed quiz 2 on day one.
    created class to calculate calibration value.
    created test class to confirm results.

---
+++ advent-of-code-2023/src/main/java/de/personal/adventofcode2023/advent_of_code_2023/quiz_02/ExtendedCalibrationValueReader.java
@@ -0,0 +1,96 @@
+package de.personal.adventofcode2023.advent_of_code_2023.quiz_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ExtendedCalibrationValueReader {
+	private static String INPUT_PATH = "./res/aoc_quiz_01_input.txt";
+	
+	private ExtendedCalibrationValueReader() {
+		
+	}
+	
+	public static int getCalibrationValue() {
+		String line;
+		int sum = 0;
+		
+		try (BufferedReader br = Files.newBufferedReader(Paths.get(INPUT_PATH)) ) {
+			
+			while ((line = br.readLine()) != null) {
+				sum += getTwoDigitValue(transformInputString(line));
+			}
+			
+		} catch (IOException e) {
+			System.err.println("An error occurred during the input import.");
+		}
+		
+		return sum;
+	}
+	
+	public static String transformInputString(String input) {
+		String transformedInput = "";
+		
+		for (int i = 0; i < input.length(); i++) {
+			if (isNumeric(input.substring(i, i+1))) {
+				transformedInput += input.substring(i, i+1);
+			} else {
+				try {
+					if (i+3 <= input.length() && input.substring(i, i+3).equals("one")) {
+						transformedInput += "1";
+						i += 1;
+					} else if (i+3 <= input.length() && input.substring(i, i+3).equals("two")) {
+						transformedInput += "2";
+						i += 1;
+					} else if (i+5 <= input.length() && input.substring(i, i+5).equals("three")) {
+						transformedInput += "3";
+						i += 3;
+					} else if (i+4 <= input.length() && input.substring(i, i+4).equals("four")) {
+						transformedInput += "4";
+						i += 2;
+					} else if (i+4 <= input.length() && input.substring(i, i+4).equals("five")) {
+						transformedInput += "5";
+						i += 2;
+					} else if (i+3 <= input.length() && input.substring(i, i+3).equals("six")) {
+						transformedInput += "6";
+						i += 1;
+					} else if (i+5 <= input.length() && input.substring(i, i+5).equals("seven")) {
+						transformedInput += "7";
+						i += 3;
+					} else if (i+5 <= input.length() && input.substring(i, i+5).equals("eight")) {
+						transformedInput += "8";
+						i += 3;
+					} else if (i+4 <= input.length() && input.substring(i, i+4).equals("nine")) {
+						transformedInput += "9";
+						i += 2;
+					}
+					
+				} catch (StringIndexOutOfBoundsException e) {
+					System.err.println("Error occurred during String transformation.");
+				}
+			}
+		}
+		
+		return transformedInput;
+	}
+	
+	public static int getTwoDigitValue(String input) {
+		String[] inputArray = input.split("");
+
+		if (!input.isEmpty()) {
+			return Integer.parseInt(inputArray[0] + inputArray[inputArray.length - 1]);
+		} else {
+			return 0;
+		}
+	}
+	
+	private static boolean isNumeric(String input) {
+		try {
+			Integer.parseInt(input);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+}
---
+++ advent-of-code-2023/src/test/java/de/personal/adventofcode2023/advent_of_code_2023/quiz_02/ExtendedCalibrationValueReaderTest.java
@@ -0,0 +1,56 @@
+package de.personal.adventofcode2023.advent_of_code_2023.quiz_02;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ExtendedCalibrationValueReaderTest {
+	
+	@BeforeEach
+	public void setup() {
+		System.out.println("ExtendedCalibrationValueReader Test starts!");
+	}
+	
+	@Test
+	@DisplayName("Get the calibration value")
+	public void testGetCalibratiomValue() {
+		Assertions.assertEquals(55291, ExtendedCalibrationValueReader.getCalibrationValue());
+	}
+	
+	@Test
+	@DisplayName("Transform input string")
+	public void testTransformInputString() {
+		Assertions.assertEquals("219",
+				ExtendedCalibrationValueReader.transformInputString("two1nine"));
+		Assertions.assertEquals("823",
+				ExtendedCalibrationValueReader.transformInputString("eightwothree"));
+		Assertions.assertEquals("2134",
+				ExtendedCalibrationValueReader.transformInputString("xtwone3four"));
+	}
+	
+	@Test
+	@DisplayName("Get two-digit value from string")
+	public void testGetTwoDigitValue() {
+		Assertions.assertEquals(29, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("two1nine")));
+		Assertions.assertEquals(83, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("eightwothree")));
+		Assertions.assertEquals(13, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("abcone2threexyz")));
+		Assertions.assertEquals(24, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("xtwone3four")));
+		Assertions.assertEquals(42, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("4nineeightseven2")));
+		Assertions.assertEquals(14, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("zoneight234")));
+		Assertions.assertEquals(76, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("7pqrstsixteen")));
+		Assertions.assertEquals(74, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("7nine4")));
+		Assertions.assertEquals(81, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("8451")));
+		Assertions.assertEquals(79, ExtendedCalibrationValueReader.getTwoDigitValue(
+				ExtendedCalibrationValueReader.transformInputString("sevenine")));
+	}
+}
