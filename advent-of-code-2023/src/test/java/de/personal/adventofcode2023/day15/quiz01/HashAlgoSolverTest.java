a12ecb873fd42d350340f474e87bff7f323926a9
Hetterle, Roman (SE-A/35) <roman.hetterle@volkswagen.de>
2023-12-15 17:58:09 +0100

    commit completed quiz 01 on day 15.

---
+++ advent-of-code-2023/src/test/java/de/personal/adventofcode2023/day15/quiz01/HashAlgoSolverTest.java
@@ -0,0 +1,37 @@
+package de.personal.adventofcode2023.day15.quiz01;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HashAlgoSolverTest {
+	
+	private String testString = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
+	private List<String> testData;
+	private List<String> realData;
+	
+	@BeforeEach
+	public void setup() {
+		this.testData = new ArrayList<>();
+		this.testData.addAll(Arrays.asList(this.testString.split(",")));
+
+		this.realData = new ArrayList<>();
+		this.realData = HashAlgoSolver.importAllData("./res/aoc_day15_input.txt");
+	}
+	
+	@Test
+	public void testApplyHashAlgorithm() {
+		Assertions.assertEquals(52, HashAlgoSolver.applyHashAlgorithm("HASH"));
+	}
+	
+	@Test
+	public void testCalculateSum() {
+		Assertions.assertEquals(1320, HashAlgoSolver.calculateSum(this.testData));
+		Assertions.assertEquals(516657, HashAlgoSolver.calculateSum(this.realData));
+	}
+	
+}
